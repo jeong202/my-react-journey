71b3d0c4b9808f4482ae0095d8ea7d6a9553c64d
jeong202 <144635664+jeong202@users.noreply.github.com>
2023-12-16 01:26:20 +0900

    20231216/오전1시/4강
    20231216/오전1시/4강

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import "./App.css";
-// HMR(hot module replacement) : App.js 작성수정조정한 코드는 브라우저에 바로 반영이 된다
+import Hello from "./component/Hello";
+import Welcome from "./component/Welcome";
 function App() {
-  const name = "테스터";
-  const naver = {
-    //객체를 변수에 할당
-    name: "네이버",
-    url: "https://www.naver.com/",
-  };
   return (
     <div className="App">
-      {/* class가 자바스크립트 예약어라서 className으로 적어준다 */}
-      {/* style는 객체로 전달해야힌다,카멜표기법이 사용된다 */}
-      <h1
-        style={{
-          color: "red",
-          backgroundColor: "#f0f",
-        }}
-      >
-        {/* 변수는 중괄호 안에 적는다 */}
-        안녕하세요.{name} {2 + 9}살입니다.
-      </h1>
-      {/* 객체도 불러올 수 있다 */}
-      <a href={naver.url}>{naver.name}</a>
+      {/* 중간에 들어갈 내용이 없으면 셀프 클로즈 해주는게 보기 좋다 */}
+      <Hello />
+      <Hello />
+      <Hello />
+      {/* 컴포넌트는 여러번 재사용이 가능하다 */}
+      <Welcome></Welcome>
     </div>
+    // app 컴포넌트 안에 헬로와 웰컴 컴포넌트가 들어가 있고 헬로 컴포넌트 안에는 월드 컴포넌트가 들어가 있다
   );
 }
 
---
+++ src/component/Hello.js
@@ -0,0 +1,26 @@
+// 방식 1 함수 기본식
+// const Hello = function () {
+//   <p>Hello</p>;
+// };
+// export default Hello;
+
+// 방식2 화살표 함수
+// const Hello = () => {
+//   <p>Hello</p>;
+// };
+// export default Hello;
+
+// 방식3 한줄
+import World from "./World";
+
+export default function Hello() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <World />
+      <World />
+      {/* 컴포넌트는 여러번 재사용이 가능하다 */}
+    </div>
+    // div나 빈 태그(<></>)없으면 에러가 남 왜냐하면 jsx는 하나의 태그만 만들 수 있다.div가 없다면 두개의 태그로 구성되는 거니 하나의 태그로 감싸준다
+  );
+}
